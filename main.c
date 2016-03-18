3ada819775575c978d9d59a34de6ecbf50d453da
Abhay Cn <abhaync@gmail.com>
2016-03-18 23:05:11 +0530

    Tweaks

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "include/functions.h"
 #include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <stdlib.h>
 
-// int windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
-// int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+//int windowHeight = glutGet(GLUT_SCREEN_HEIGHT);
+//int windowWidth = glutGet(GLUT_SCREEN_WIDTH);
 
 
 /* Function Description:
@@ -11,14 +13,17 @@
  *
  */
 void initialize() {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	gluOrtho2D(0.0,499.0,0.0,499.0);
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPointSize(5.0);
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POINTS);
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
 		glVertex2f(115.0, 115.0);
 		glVertex2f(215.0, 215.0);
 	glEnd();
@@ -46,13 +51,16 @@ void keyPressed(unsigned char key, int x, int y) {
 
 int main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_ALPHA|GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 	glutCreateWindow("CG-Project");
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	initialize();
 	glutFullScreen();
 	glutDisplayFunc(display);
 	glutMouseFunc(mouseClicked);
-	glutKeyboardFunc(keyPressed);
-	initialize();
+	//glutKeyboardFunc(keyPressed);
+	//initialize();
 	glutMainLoop();
 	return 0;
 }
